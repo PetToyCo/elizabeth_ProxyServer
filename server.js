befc7a2ead3ea9952e437467600937e2b73dddf7
Elizabeth Hayt <enhayt@gmail.com>
2020-06-30 12:36:43 -0700

    Basic server and html setup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('./product', (req, res) => {
+  var itemId = req.query;
+  console.log('itemId: ', itemId);
+
+  
+})
 
 app.listen(3000, () => {
   console.log('Proxy server is listening on PORT 3000');
