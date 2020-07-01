42a9a08f29876d649f94eb190c91dbff69fb7b3f
Elizabeth Hayt <enhayt@gmail.com>
2020-06-30 18:58:02 -0700

    Finish /product route in server

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('./product', (req, res) => {
-  var itemId = req.query;
-  console.log('itemId: ', itemId);
+app.get('/product', (req, res) => {
+  var itemId = req.query.itemId;
+  var itemNum = Number.parseInt(itemId);
+  console.log('itemNum: ', itemNum);
 
-  
+  if (itemNum <= 199 && itemNum >= 100) {
+    res.sendFile(`${__dirname}/public/index.html`);
+  } else {
+    res.status(404).send();
+  }
+
 })
 
 app.listen(3000, () => {
